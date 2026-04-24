70da9462852e50b366bf7668412184cc445c51f3
Prashansasharma14 <prashansasharma14.ps@gmail.com>
2026-04-24 17:31:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,14 @@ if st.button("🔍 Predict Risk"):
     # Prediction with spinner
     with st.spinner("Analyzing patient risk..."):
         prob = model.predict_proba(input_data)[0][1]
-
+   st.markdown("### 🧾 Patient Summary")(f"""
+Patient Summary:
+- Age: {age}
+- Hb: {hb}
+- BMI: {bmi}
+- Hypertension: {'Yes' if bp else 'No'}
+- Previous PPH: {'Yes' if prev_pph else 'No'}
+""") 
     # Clinical flags
     if hb < 7:
         st.warning("Severe anemia detected — high clinical concern.")
